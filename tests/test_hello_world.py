85f1167117ad3d66410ad43ecb52037a21d2bf20
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-08-29 18:42:32 +0200

    feat: write status on assignment for week1

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,8 +1,6 @@
 from modules.hello_world import get_hello_world
 
-class TestHelloWorld():
 
+class TestHelloWorld:
     def test_get_hello_world(self):
         assert get_hello_world() == "Hello world"
-
-
--- tests/test_something.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture(autouse=True)
-def run_before_and_after_tests():
-    # Setup
-    yield
-    # Teardown
