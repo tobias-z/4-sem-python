0edf2945965e4b57e3209ba1ea6a3bf11486d52a
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-08-29 21:02:44 +0200

    test: setup examples of testing in python

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,6 +1,34 @@
 from modules.hello_world import get_hello_world
+import pytest
+
+
+#################################################################################
+# Class way of testing
 
 
 class TestHelloWorld:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
     def test_get_hello_world(self):
         assert get_hello_world() == "Hello world"
+
+
+#################################################################################
+# None class way of testing
+
+
+@pytest.fixture(autouse=True)
+def run_before_and_after_tests():
+    # Setup
+    pass
+    yield
+    # Teardown
+    pass
+
+
+def test_something():
+    assert True is True
