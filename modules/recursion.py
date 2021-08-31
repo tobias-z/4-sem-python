c5a7ea6741b32bd7caa0a2f5c3c62e7354f960ac
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-08-31 12:57:49 +0000

    chore: add recursion example

---
+++ modules/recursion.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+
+my_people = [
+    {
+        "name": "Bob",
+        "children": [
+            {
+                "name": "bob child",
+                "children": [
+                    {"name": "bob child's child", "children": []},
+                ],
+            },
+            {"name": "bob child2", "children": []},
+        ],
+    },
+    {
+        "name": "Tobias",
+        "children": [
+            {"name": "Tobias child", "children": []},
+            {"name": "Tobias child2", "children": []},
+        ],
+    },
+]
+
+
+def print_people(people: "list[dict[str, Any]]"):
+    if len(people) == 0:
+        return
+    for person in people:
+        print(person.get("name"))
+        print_people(person.get("children"))
+
+
+if __name__ == "__main__":
+    print_people(my_people)
